a15ff2f72d90db2c185a529b150c7bd89db73267
Ryan <njuyyzh@gmail.com>
2014-05-17 15:41:30 -0400

    final

--- src/info/androidhive/slidingmenu/CommunityFragment.java
+++ src/info/androidhive/slidingmenu/CommunityFragment.java
@@ -15,47 +15,58 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 public class CommunityFragment extends Fragment {
-	
-	public CommunityFragment(){}
-	
+
+	public CommunityFragment() {
+	}
+
 	@Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState) {
- 
-        View rootView = inflater.inflate(R.layout.contact_list, container, false);
-        ArrayList<Integer> list = new ArrayList<Integer>();
-        list.add(1);
-        list.add(2);
-        list.add(1);
-        list.add(2);
-        list.add(1);
-        list.add(2);
-        list.add(1);
-        list.add(2);
-        list.add(1);
-        list.add(2);
-        list.add(1);
-        list.add(2);
-        list.add(1);
-        list.add(2);
-        list.add(1);
-        list.add(2);
-        list.add(1);
-        list.add(2);
-        list.add(1);
-        list.add(2);
-        MyListAdapter mAdapter = new MyListAdapter(getActivity(),list);
-        SwingBottomInAnimationAdapter adapter = new SwingBottomInAnimationAdapter(mAdapter);
-        ListView lv = (ListView) getActivity().findViewById(R.id.contact_list);
-        adapter.setAbsListView(lv);
-        lv.setAdapter(adapter);
-        return rootView;
-    }
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+
+		View rootView = inflater.inflate(R.layout.contact_list, container,
+				false);
+
+		return rootView;
+	}
+
+	@Override
+	public void onStart() {
+		super.onStart();
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		list.add(1);
+		list.add(2);
+		list.add(1);
+		list.add(2);
+		list.add(1);
+		list.add(2);
+		list.add(1);
+		list.add(2);
+		list.add(1);
+		list.add(2);
+		list.add(1);
+		list.add(2);
+		list.add(1);
+		list.add(2);
+		list.add(1);
+		list.add(2);
+		list.add(1);
+		list.add(2);
+		list.add(1);
+		list.add(2);
+		MyListAdapter mAdapter = new MyListAdapter(getActivity(), list);
+		SwingBottomInAnimationAdapter adapter = new SwingBottomInAnimationAdapter(
+				mAdapter);
+		ListView lv = (ListView) getActivity().findViewById(R.id.contact_list);
+		adapter.setAbsListView(lv);
+		lv.setAdapter(adapter);
+	}
+
 	private static class MyListAdapter extends ArrayAdapter<Integer> {
 
 		private final Context mContext;
 
-		public MyListAdapter(final Context context, final ArrayList<Integer> items) {
+		public MyListAdapter(final Context context,
+				final ArrayList<Integer> items) {
 			super(items);
 			mContext = context;
 		}
@@ -71,10 +82,12 @@ public class CommunityFragment extends Fragment {
 		}
 
 		@Override
-		public View getView(final int position, final View convertView, final ViewGroup parent) {
+		public View getView(final int position, final View convertView,
+				final ViewGroup parent) {
 			TextView tv = (TextView) convertView;
 			if (tv == null) {
-				tv = (TextView) LayoutInflater.from(mContext).inflate(R.layout.list_row, parent, false);
+				tv = (TextView) LayoutInflater.from(mContext).inflate(
+						R.layout.list_row, parent, false);
 			}
 			tv.setText("This is row number " + getItem(position));
 			return tv;
--- src/info/androidhive/slidingmenu/MainActivity.java
+++ src/info/androidhive/slidingmenu/MainActivity.java
@@ -68,8 +68,8 @@ public class MainActivity extends Activity {
 		// navDrawerItems.add(new NavDrawerItem(navMenuTitles[2],
 		// navMenuIcons.getResourceId(2, -1)));
 		// Communities, Will add a counter here
-		navDrawerItems.add(new NavDrawerItem(navMenuTitles[2], navMenuIcons
-				.getResourceId(3, -1), true, "22"));
+		navDrawerItems.add(new NavDrawerItem(navMenuTitles[3], navMenuIcons
+				.getResourceId(2, -1), true, "22"));
 		// Pages
 		// navDrawerItems.add(new NavDrawerItem(navMenuTitles[4],
 		// navMenuIcons.getResourceId(4, -1)));

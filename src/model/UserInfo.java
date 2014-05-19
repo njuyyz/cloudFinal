7db4efeff3a7ed100916b4e592bac98e72498926
Ryan <njuyyzh@gmail.com>
2014-05-18 21:13:34 -0400

    change bg

--- src/model/UserInfo.java
+++ src/model/UserInfo.java
@@ -18,15 +18,14 @@ import com.google.code.linkedinapi.schema.PhoneNumbers;
 
 public class UserInfo {
 
-	
 	private String id;
 	private String firstName;
 	private String lastName;
 	private String headline;
 	private String[] educationList;
-	private String[] phoneList; 
+	private String[] phoneList;
 	private String industry;
-//	private String[] honorList; 
+	// private String[] honorList;
 	private String summary;
 	private String dateOfBirth;
 	private String currentStatus;
@@ -36,48 +35,46 @@ public class UserInfo {
 	private String videoUrl;
 	private String audioUrl;
 	private String styleUrl;
-	
+
 	private Bitmap thumbNail;
-	
-	
 
 	public Bitmap getThumbNail() {
 		return thumbNail;
 	}
 
-
 	public void setThumbNail(Bitmap thumbNail) {
 		this.thumbNail = thumbNail;
 	}
 
-
-	public UserInfo(JSONObject jsonObject){
-		 firstName = (String)jsonObject.get("first_name");
-		 lastName = (String)jsonObject.get("last_name");
-		 headline = (String)jsonObject.get("headline");
-		 String educations = (String)jsonObject.get("educations");
-		 educationList = educations.split(":");
-		 String phoneNumbers = (String)jsonObject.get("phone_numbers");
-		 phoneList = phoneNumbers.split(":");
-		 industry = (String)jsonObject.get("industry");
-//		 String honors = (String)jsonObject.get("honors");
-//		 honorList = honors.split(":");
-		 summary = (String)jsonObject.get("summary");
-		 dateOfBirth = (String)jsonObject.get("date_of_birth");
-		 currentStatus = (String)jsonObject.get("current_status");
-		 String languages = (String)jsonObject.get("languages");
-		 languageList = languages.split(":");
-		 mainAddress = (String)jsonObject.get("main_address");
-		 picUrl = (String)jsonObject.get("picture_url");
-		 videoUrl = (String)jsonObject.get("video_url");
-		 audioUrl = (String)jsonObject.get("audio_url");
-		 styleUrl = (String)jsonObject.get("style_id");
-		 id = (String)jsonObject.get("u_id");
+	public UserInfo(JSONObject jsonObject) {
+		firstName = (String) jsonObject.get("first_name");
+		lastName = (String) jsonObject.get("last_name");
+		headline = (String) jsonObject.get("headline");
+		String educations = (String) jsonObject.get("educations");
+		educationList = educations.split(":");
+		String phoneNumbers = (String) jsonObject.get("phone_numbers");
+		phoneList = phoneNumbers.split(":");
+		industry = (String) jsonObject.get("industry");
+		// String honors = (String)jsonObject.get("honors");
+		// honorList = honors.split(":");
+		summary = (String) jsonObject.get("summary");
+		dateOfBirth = (String) jsonObject.get("date_of_birth");
+		currentStatus = (String) jsonObject.get("current_status");
+		String languages = (String) jsonObject.get("languages");
+		languageList = languages.split(":");
+		mainAddress = (String) jsonObject.get("main_address");
+		picUrl = (String) jsonObject.get("picture_url");
+		videoUrl = (String) jsonObject.get("video_url");
+		audioUrl = (String) jsonObject.get("audio_url");
+		styleUrl = (String) jsonObject.get("style_id");
+		if (styleUrl == null || styleUrl.equals("null")) {
+			styleUrl = "0";
+		}
+		id = (String) jsonObject.get("u_id");
 	}
 
-	
 	@SuppressWarnings("unchecked")
-	public JSONObject convertToJson(){
+	public JSONObject convertToJson() {
 		JSONObject object = new JSONObject();
 		object.put("u_id", id);
 		object.put("first_name", firstName);
@@ -85,47 +82,47 @@ public class UserInfo {
 		object.put("headline", headline);
 		StringBuffer educationBuffer = new StringBuffer();
 		for (int i = 0; i < educationList.length; i++) {
-			educationBuffer.append( educationList[i]+":" );
-		   //result.append( optional separator );
+			educationBuffer.append(educationList[i] + ":");
+			// result.append( optional separator );
 		}
-		String educationString = educationBuffer.substring(0, educationBuffer.length()-1);
-		object.put("educations",educationString);
-		
+		String educationString = educationBuffer.substring(0,
+				educationBuffer.length() - 1);
+		object.put("educations", educationString);
 
 		StringBuffer phoneBuffer = new StringBuffer();
 		for (int i = 0; i < phoneList.length; i++) {
-			phoneBuffer.append( phoneList[i]+":" );
-		   //result.append( optional separator );
+			phoneBuffer.append(phoneList[i] + ":");
+			// result.append( optional separator );
 		}
-		String phoneString = phoneBuffer.substring(0, phoneBuffer.length()-1);
-		object.put("phone_numbers",phoneString);
-		
+		String phoneString = phoneBuffer.substring(0, phoneBuffer.length() - 1);
+		object.put("phone_numbers", phoneString);
+
 		object.put("industry", industry);
 		object.put("honors", null);
 		object.put("summary", summary);
 		object.put("date_of_birth", dateOfBirth);
-		object.put("current_status",currentStatus);
+		object.put("current_status", currentStatus);
 
 		StringBuffer languageBuffer = new StringBuffer();
 		for (int i = 0; i < languageList.length; i++) {
-			languageBuffer.append( languageList[i]+":" );
-		   //result.append( optional separator );
+			languageBuffer.append(languageList[i] + ":");
+			// result.append( optional separator );
 		}
-		String languageString = languageBuffer.substring(0, languageBuffer.length()-1);
-		object.put("languages",languageString);
-		
+		String languageString = languageBuffer.substring(0,
+				languageBuffer.length() - 1);
+		object.put("languages", languageString);
+
 		object.put("main_address", mainAddress);
 		object.put("picture_url", picUrl);
 		object.put("video_url", videoUrl);
 		object.put("audio_url", audioUrl);
 		object.put("style_id", styleUrl);
-		
+
 		return object;
 	}
-	
-	public UserInfo(Person profile){
-		
-	
+
+	public UserInfo(Person profile) {
+
 		firstName = profile.getFirstName();
 		lastName = profile.getLastName();
 		headline = profile.getHeadline();
@@ -133,27 +130,27 @@ public class UserInfo {
 		List<Education> educationss = educations.getEducationList();
 		ArrayList<String> schoolList = new ArrayList<String>();
 		educationList = new String[educationss.size()];
-		for(Education e: educationss){
+		for (Education e : educationss) {
 			schoolList.add(e.getSchoolName());
 		}
 		educationList = schoolList.toArray(educationList);
-		
+
 		PhoneNumbers pns = profile.getPhoneNumbers();
 		List<PhoneNumber> pnList = pns.getPhoneNumberList();
 		ArrayList<String> phoneNumberList = new ArrayList<String>();
 		phoneList = new String[phoneNumberList.size()];
-		for(PhoneNumber pn: pnList){
+		for (PhoneNumber pn : pnList) {
 			phoneNumberList.add(pn.getPhoneNumber());
 		}
 		phoneList = phoneNumberList.toArray(phoneList);
-		
+
 		industry = profile.getIndustry();
-		
-//		honors = profile.getHonors();
-		
+
+		// honors = profile.getHonors();
+
 		summary = profile.getSummary();
 		DateOfBirth dob = profile.getDateOfBirth();
-		if(dob!=null){
+		if (dob != null) {
 			long day = dob.getDay();
 			long month = dob.getMonth();
 			long year = dob.getYear();
@@ -164,7 +161,7 @@ public class UserInfo {
 		List<Language> languagess = languages.getLanguageList();
 		ArrayList<String> ll = new ArrayList<String>();
 		languageList = new String[ll.size()];
-		for( Language l : languagess){
+		for (Language l : languagess) {
 			ll.add(l.getLanguage().getName());
 		}
 		languageList = ll.toArray(languageList);
@@ -172,12 +169,11 @@ public class UserInfo {
 		picUrl = profile.getPictureUrl();
 		videoUrl = null;
 		audioUrl = null;
-		styleUrl = null;
+		styleUrl = "0";
 		id = profile.getId();
-		
-		
+
 	}
-	
+
 	public String getId() {
 		return id;
 	}
@@ -306,5 +302,4 @@ public class UserInfo {
 		this.styleUrl = styleUrl;
 	}
 
-	
 }
